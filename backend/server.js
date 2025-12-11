7c94c80173bbe09155addd51eff3458484932cc5
tharun <tharunbollam66@gmail.com>
2025-12-11 16:39:00 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,11 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
+
+app.use(cors({
+  origin: ['https://acron-assignment-2qsb.vercel.app/', 'http://localhost:5174'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Database connection
