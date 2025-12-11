2b504eac2cfe82103556acf85a21c08b6e4534fb
tharun <tharunbollam66@gmail.com>
2025-12-11 16:51:01 +0530

    fix: update CORS for production deployment

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,9 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+// Middleware
 app.use(cors({
-  origin: ['https://acron-assignment-2qsb.vercel.app/', 'http://localhost:5174'],
+  origin: ['https://acron-assignment.vercel.app', 'http://localhost:5174'],
   credentials: true
 }));
 app.use(express.json());
